954ff417beb5721d91b53ca0b7a4999328fc650f
Arda Ekinci <ardaekinci@valensas.com>
2020-06-04 00:51:24 +0300

    add loop mechanism for ui controller

--- ui.hs
+++ ui.hs
@@ -1,4 +1,5 @@
 import Data.Char
+import Control.Monad
 
 uiOptions = "a) View a Country's Ninja Information \n\
             \b) View All Countries' Ninja Information \n\
@@ -6,27 +7,61 @@ uiOptions = "a) View a Country's Ninja Information \n\
             \d) Make a Round Between Countries \n\
             \e) Exit"
 actionMsg = "Enter the action: "
-availableActions = ['a'..'e']
 countryMsg = "Enter the country code: "
+invalidCountryInput = "Invalid Country entered"
+availableActions = ['a'..'e']
 availableCountries = "eElLwWnNfF"
-availableActions = 
+availableNinjas = ["Naruto", "Haruki"] -- will be filled from txt
+
+{-
+getNinjasByCountry :: Char -> IO String
+getNinjasByCountry country = do
+    return ("Ninjas of ..")
+
+listNinjasByCountry :: IO()
+listNinjasByCountry = do
+    putStr countryMsg
+    country <- getLine
+    if (selectAction country)
+        then getNinjasByCountry (head country)
+        else putStrLn invalidCountryInput
+
+makeRoundBetweenNinjas :: IO() String
+makeRoundBetweenNinjas = do
+    putStr "Enter the name of the first ninja: "
+    firstNinja <- getLine
+    putStr "Enter the country code of the first ninja: "
+    countryOfFirstNinja <- getLine
+    if (isCountryValid countryOfFirstNinja)
+        then return invalidCountryInput
+    putStrLn "Success"
+-}
+
+uiController :: IO()
 uiController = do
-    putStrLn uiOptions
-    putStr actionMsg
-    action <- getLine
-    putStrLn "hello"
+    let uiLoop = do
+        putStrLn uiOptions
+        putStr actionMsg
+        action <- getLine
+        selectAction action
+        when (action /= "e") uiLoop -- Do not break the loop until the user want to exit
+    uiLoop -- Start first loop
+
+selectAction :: String -> IO()
+selectAction "a" = putStrLn "List Ninjas"
+selectAction "b" = putStrLn "List All Ninjas"
+selectAction "c" = putStrLn "Round Ninjas"
+selectAction "d" = putStrLn "Round Countries"
+selectAction "e" = putStrLn "Exit"
+selectAction _ = putStrLn "Invalid Action entered"
 
 -- Pattern Matching
-isActionValid :: String -> Bool
-isActionValid [action] = elem action availableActions -- Matches on exactly one item with this pattern 
-isActionValid _ = False -- Return False for other inputs
+-- | The 'isCountryValid' function checks the user input for validation.
+-- param1 is String and user input.
+isCountryValid :: String -> Bool 
+isCountryValid [country] = elem country availableCountries -- Matches on exactly one item for a country with this pattern 
+isCountryValid _ = False -- Return False for other inputs
 
 
 main = do  
-    putStrLn "What's your first name?"  
-    firstName <- getLine  
-    putStrLn "What's your last name?"  
-    lastName <- getLine  
-    let bigFirstName = map toUpper firstName  
-        bigLastName = map toUpper lastName  
-    putStrLn $ "hey " ++ bigFirstName ++ " " ++ bigLastName ++ ", how are you?"  
+    uiController -- Starts the ui controller
