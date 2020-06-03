784620236b586ade5a68926f4bba721a6ac417d5
Arda Ekinci <ardaekinci@valensas.com>
2020-06-03 23:19:26 +0300

    add validation function for actions by pattern matching

--- ui.hs
+++ ui.hs
@@ -5,8 +5,21 @@ uiOptions = "a) View a Country's Ninja Information \n\
             \c) Make a Round Between Ninjas \n\
             \d) Make a Round Between Countries \n\
             \e) Exit"
+actionMsg = "Enter the action: "
+availableActions = ['a'..'e']
+countryMsg = "Enter the country code: "
+availableCountries = "eElLwWnNfF"
+availableActions = 
 uiController = do
     putStrLn uiOptions
+    putStr actionMsg
+    action <- getLine
+    putStrLn "hello"
+
+-- Pattern Matching
+isActionValid :: String -> Bool
+isActionValid [action] = elem action availableActions -- Matches on exactly one item with this pattern 
+isActionValid _ = False -- Return False for other inputs
 
 
 main = do  
